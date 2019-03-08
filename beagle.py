1131cd0b42271a4d4d012c5a2e4dfefe7c5acb09
mez0@pm.me <mez0@pm.me>
2019-03-08 21:52:41 +0000

    Host discovery sorted

--- beagle.py
+++ beagle.py
@@ -1,54 +1,124 @@
 #!/usr/bin/python3
-
-'''
-Beagle's objectives:
-	1. Identify SMB interactions and list shares
-	2. Identify SMB Security
-	3. Identify RPC Interactions.
-		3a. This will be the --discover flag from Ridder. Ridder will stay as an enumeration tool and Beagle will adopt all discovery functionality.
-'''
-
+import argparse, os.path
+from netaddr import IPNetwork
+from lib import logger
 from smb.SMBConnection import SMBConnection
-import argparse
-parser = argparse.ArgumentParser(description="Get shares for machine")
-parser.add_argument("-t", "--target", required=True, metavar="", help="Host to enumerate")
-parser.add_argument("-u", "--username", type=str, metavar="", help="Username to connect with")
-parser.add_argument("-p", "--password", metavar="", help="Password to connect with")
-parser.add_argument("-d", "--domain", metavar="", help="Domain to connect with")
-parser.add_argument("-r", "--recursive", action="store_true", help="Recursively look through shares")
+from scapy.all import *
+
+parser = argparse.ArgumentParser(description="Release the bagels.")
+parser.add_argument("-t", "--target", required=True, metavar="", help="Takes single IP, CIDR or file.")
+parser.add_argument("-d", "--discovery-mode", metavar="", help="The method for checking if the host is up")
+parser.add_argument("-e", "--enumerate", metavar="", help="What the bagels are looking for.")
 args = parser.parse_args()
 
-if args.username:
-	username = args.username
-else:
-	username=""
+def get_targets(targets):
 
-if args.password:
-	password = args.password
-else:
-	password=""
+	target_list=[]
 
-client_machine_name = 'dc-win12'
-server_name = 'servername'
-server_ip = args.target
+	if os.path.isfile(targets) == True:
+		with open(targets,'r') as file:
+			contents=file.readlines()
+			for i in (contents):
+				target=i.rstrip()
+				target_list.append(target)
+			return target_list
+	else:
+		if "/" in targets:
+			try:
+				subnet=IPNetwork(targets)
+			except:
+				print('failed to parse')
+				quit()
 
-if args.domain:
-	domain_name = args.domain
-else:
-	domain_name = "WORKGROUP"
+			for i in subnet:
+				tmp_str=str(i)
+				last_octet=str(tmp_str.split('.')[3])
+				if last_octet == '0' or last_octet == '255':
+					pass
+				else:
+					target_list.append(str(i))
+			return target_list		
 
-conn = SMBConnection(username, password,client_machine_name,server_name,domain_name,use_ntlm_v2=True,is_direct_tcp=True)
-conn.connect(server_ip, 445)
+		else:
+			target_list.append(targets)
+			return target_list
 
-shares = conn.listShares(timeout=30)  # obtain a list of shares
-for i in range(len(shares)):  # iterate through the list of shares
-	print(shares[i].name)
+def tcp_scan(targets,ports):
+	src_port = RandShort()
+	FIN = 0x01
+	SYN = 0x02
+	RST = 0x04
+	PSH = 0x08
+	ACK = 0x10
+	SYNACK = 0x12
+	RSTACK = 0x14
+	URG = 0x20
+	ECE = 0x40
+	CWR = 0x80
 
-if args.recursive:
-	for i in range(len(shares)):
-		files = conn.listPath(shares[i].name,'/',timeout=30)
-		print(client_machine_name + ':' + shares[i].name)
-		for i in range(len(files)):
-			print('\t'+files[i].filename)
+	alive_hosts=[]
 
-conn.close()
+	for target in targets:
+		logger.blue('Checking TCP ports: {}'.format(target))
+		for port in ports:
+			send_syn = sr1(IP(dst=target)/TCP(sport=src_port,dport=port,flags=SYN),verbose=0,timeout=2)
+			if send_syn == None:
+				pass
+			elif(send_syn.haslayer(TCP)):
+				if(send_syn.getlayer(TCP).flags == SYNACK):
+					send_ack = sr(IP(dst=target)/TCP(sport=src_port,dport=port,flags=RST),verbose=0,timeout=2)
+					logger.green_indent('{}:{} OPEN'.format(target,port))
+					if target not in alive_hosts:
+						alive_hosts.append(target)
+				elif (send_syn.getlayer(TCP).flags == RSTACK):
+					pass
+				elif (send_syn.getlayer(TCP).flags == RST):
+					pass
+	return alive_hosts
+
+
+def icmp_scan(targets):
+	alive_hosts=[]
+	for target in targets:
+		logger.blue('Pinging: {}'.format(target))
+		resp = sr1(IP(dst=str(target))/ICMP(),timeout=2,verbose=0)
+		if resp is None:
+			pass
+		elif(int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+			pass
+		else:
+			logger.green_indent('{}: Up'.format(target))
+			if target not in alive_hosts:
+				alive_hosts.append(target)
+	return alive_hosts
+
+
+t=get_targets(args.target)
+p=[53,88,139,445,464]
+
+
+logger.blue('Found {} targets'.format(len(t)))
+
+#The iteration will be done in the enumeratio functions. Even if the list has one ip, this discovery will still work fine. Might be worth grabbing the len of targets when passed into tcp_scan to check properly.
+if args.discovery_mode.lower() == 'port':
+	alive_hosts=tcp_scan(t,p)
+
+elif args.discovery_mode.lower() == 'icmp':
+	alive_hosts=icmp_scan(t)
+
+elif args.discovery_mode == 'skip':
+	alive_hosts=t
+
+# if args.enumerate == 'null':
+# 	hunt_null(t)
+# elif args.enumerate == 'shares':
+# 	hunt_shares(t)
+# elif args.enumerate == 'ms17-010':
+# 	hunt_ms17(t)
+# elif args.enumerate == 'all':
+# 	hunt_null(t)
+# 	hunt_shares(t)
+# 	hunt_ms17(t)
+# else:
+# 	print('the fuck')
+
---
+++ lib/logger.py
@@ -0,0 +1,32 @@
+from time import gmtime, strftime
+
+colour_red = "\033[1;31m"
+colour_blue = "\033[1;34m"
+colour_green = "\033[1;32m"
+colour_remove= "\033[0m"
+good='[+]'
+bad='[!]'
+info='[#]'
+
+def RED(string):
+	return (colour_red + string + colour_remove)
+
+def BLUE(string):
+	return (colour_blue + string + colour_remove)
+
+
+def GREEN(string):
+	return (colour_green + string + colour_remove)
+
+def blue(string):
+	print(BLUE(info)+'\t'+string)
+
+def red(string):
+	print(RED(bad)+'\t'+string)
+
+def green(string):
+	print(GREEN(good)+'\t'+string)
+
+def green_indent(string):
+	print('\t'+GREEN(good)+'\t'+string)
+
